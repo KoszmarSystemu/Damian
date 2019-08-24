86de0ef342e1e1738c2db360223fb5f4f4513fb3
damia <damiangoldik@gmail.com>
2019-08-24 15:14:55 +0200

    repository init

--- src/main/java/pl/sdaacademy/datastructures.java
+++ src/main/java/pl/sdaacademy/datastructures.java
@@ -1,7 +1,25 @@
 package pl.sdaacademy;
 
+import pl.sdaacademy.queue.ArrayQueue;
+import pl.sdaacademy.queue.Queue;
+
 public class datastructures {
     public static void main(String[] args) {
+        Queue<String> stringQueue = new ArrayQueue<>();
+        stringQueue.enqueue( "Darek" );
+        stringQueue.enqueue(  "Stefan");
+        stringQueue.enqueue( "Jolanta" );
+        stringQueue.enqueue( "Anna" );
+        stringQueue.enqueue("Zenon"  );
 
-    }
-}
+        System.out.println(stringQueue.dequeue());
+        System.out.println(stringQueue.dequeue());
+        System.out.println(stringQueue.dequeue());
+        System.out.println(stringQueue.dequeue());
+        System.out.println(stringQueue.dequeue());
+        System.out.println(stringQueue.dequeue());
+
+
+        ArrayQueue array = new  ArrayQueue();
+
+}}
